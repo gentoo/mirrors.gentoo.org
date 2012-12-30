56048b11e8fc7d04a020f470e4c12d6e6809468e
Pavlos Ratis <dastergon@gentoo.org>
2012-12-30 20:09:10 +0000

    updated urls for index and static files

--- mirrors/urls.py
+++ mirrors/urls.py
@@ -1,17 +1,8 @@
-from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.conf.urls import patterns, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mirrors.views.home', name='home'),
-    # url(r'^mirrors/', include('mirrors.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'mirrors.views.index'),
 )
+
+urlpatterns += staticfiles_urlpatterns()
