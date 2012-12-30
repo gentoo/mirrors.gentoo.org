daaaa2b6860ddba929a0d5805d45ae948925832d
Pavlos Ratis <dastergon@gentoo.org>
2012-12-30 20:13:23 +0000

    static files {css,img,js}

---
+++ mirrors/static/js/sort.js
@@ -0,0 +1,7 @@
+$(document).ready(function()
+        {
+                // call the tablesorter plugin
+                $("#distfiles").tablesorter( {sortList: [[0,0]] });
+                $("#rsync").tablesorter( { sortList:[[0,0]]});
+        }
+);
