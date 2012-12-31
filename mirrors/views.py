50e32d5d531e8293a854f2c06ac6f0c57bfcf77e
Theo Chatzimichos <tampakrap@gentoo.org>
2012-12-31 03:13:14 +0100

    Fix names of variables

--- mirrors/views.py
+++ mirrors/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from mirrors.models import RsyncMirrors, DistfilesMirrors
 
-
 def index(request):
-    rsyncmirror = RsyncMirrors.objects.all()
-    distmirror = DistfilesMirrors.objects.all()
-    return render_to_response('index.html', {'rsyncmirror': rsyncmirror, 'distmirror': distmirror}, context_instance=RequestContext(request))
+    rsyncmirrors = RsyncMirrors.objects.all()
+    distfilesmirrors = DistfilesMirrors.objects.all()
+    return render_to_response('index.html', {
+        'rsyncmirrors': rsyncmirrors,
+        'distfilesmirrors': distfilesmirrors,
+    }, context_instance=RequestContext(request))
